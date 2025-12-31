f16b649902f95ebcd906d76b2b36e478dbf1fa82
ming_dude <mingde@gmail.com>
2025-12-31 14:35:42 +0800

    Update vect2.hpp

--- v2_basic/vect2.hpp
+++ v2_basic/vect2.hpp
@@ -17,19 +17,44 @@ public:
 	int & operator[](const int index);
 	int operator[](const int index) const;
 	
-	vect2 operator+(const int num);
-	vect2 operator-(const int num);
-	vect2 operator*(const int num);
+	vect2 operator+(const int num) const {return vect2(x + num, y + num);};
+	vect2 operator-(const int num) const {return vect2(x - num, y - num);};
+	vect2 operator*(const int num) const {return vect2(x * num, y * num);};
 
-	vect2 operator+(const vect2 & rhs);
-	vect2 operator-(const vect2 & rhs);
-	vect2 operator*(const vect2 & rhs);
+	vect2 operator+(const vect2 & rhs) const {return vect2(x + rhs.x, y + rhs.y);};
+	vect2 operator-(const vect2 & rhs) const {return vect2(x - rhs.x, y - rhs.y);};
+	vect2 operator*(const vect2 & rhs) const {return vect2(x * rhs.x, y * rhs.y);};
 	
-	bool operator==(const vect2 & rhs);
-	
-	friend std::ostream & operator<<(std::ostream & out, const vect2 & v);
+	bool operator==(const vect2 & rhs) const {return (x == rhs.x && y == rhs.y);};
+	bool operator!=(const vect2 & rhs) const {return !(operator==(rhs));};
 
+	vect2 operator-() const {return vect2(-x, -y);};
+
+	vect2 & operator--();
+	const vect2 operator--(int);
+
+	vect2 & operator++();
+	const vect2 operator++(int);
+
+	vect2 & operator+=(const int num);
+	vect2 & operator-=(const int num);
+	vect2 & operator*=(const int num);
+
+	vect2 & operator+=(const vect2 & rhs);
+	vect2 & operator-=(const vect2 & rhs);
+	vect2 & operator*=(const vect2 & rhs);
+
+	
+	int getX() const {return x;}
+	int getY() const {return y;}
+	
+	friend vect2 operator+(const int num, const vect2 & rhs) {return vect2(num + rhs.x, num + rhs.y);};
+	friend vect2 operator-(const int num, const vect2 & rhs) {return vect2(num - rhs.x, num - rhs.y);};
+	friend vect2 operator*(const int num, const vect2 & rhs) {return vect2(num * rhs.x, num * rhs.y);};
+	
 };
+
+std::ostream & operator<<(std::ostream & out, const vect2 & v);
 
 #endif
 
